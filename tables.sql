2d9cd7b1d659d50242a5091c18f325b65ab2862e
nolitha <nolithagrula@gmail.com>
2016-05-05 12:32:43 +0200

    modified tables.sql

--- tables.sql
+++ tables.sql
@@ -1,39 +1,35 @@
-DROP TABLE IF EXISTS 'users';
-CREATE TABLE users (
+DROP TABLE IF EXISTS 'employees';
+CREATE TABLE employees (
 	id int NOT NULL AUTO_INCREMENT ,
-	username VARCHAR(100) NOT NULL default '',
-	password VARCHAR(100) NOT NULL default '',
-	UNIQUE KEY (username),
-	roles VARCHAR(100) NOT NULL,
+	name VARCHAR(100) NOT NULL default '',
 	primary key(id)
 );
 
 
-DROP TABLE IF EXISTS `milestones`;
-CREATE TABLE milestones (
-	id int NOT NULL AUTO_INCREMENT,
-	exercise VARCHAR(50),
-	rewards VARCHAR(50),
-	FOREIGN KEY(user_id) REFERENCES users(id),
-	FOREIGN KEY(user_id) REFERENCES users(id),
-	primary key(id)
+-- DROP TABLE IF EXISTS `milestones`;
+-- CREATE TABLE milestones (
+-- 	milestones_id int NOT NULL AUTO_INCREMENT,
+-- 	exercise VARCHAR(50),
+-- 	rewards VARCHAR(50),
+-- 	FOREIGN KEY(Exercise_id) REFERENCES Exercise(id),
+-- 	FOREIGN KEY(employee_id) REFERENCES employees(id),
+-- 	primary key(id)
+-- );
 
+	-- DROP TABLE IF EXISTS `stats`;
+	-- CREATE TABLE stats
+	-- ( stats_id int NOT NULL,
+	-- 	stats_time int,
+	-- 	weekly_exercise_id int NOT NULL,
+	-- 	employees VARCHAR(50),
+	-- 	FOREIGN KEY(weekly_exercise_id) REFERENCES weekly_exercise(id),
+	-- 	CONSTRAINT stats_pk PRIMARY KEY (stats_id)
+	-- 	);
 
-	DROP TABLE IF EXISTS `stats`;
-	CREATE TABLE stats (
-		id int NOT NULL AUTO_INCREMENT,
-		exercise VARCHAR(50),
-		PRIMARY KEY(id),
-		FOREIGN KEY(stats_id) REFERENCES stats(id)
-
-		);
-
-		DROP TABLE IF EXISTS `weekly excercises`;
-		CREATE TABLE weekly excercises (
-			id int NOT NULL AUTO_INCREMENT,
-			excercise VARCHAR(50),
-			reps int,
-			primary key(id),
-			FOREIGN KEY(user_id) REFERENCES users(id)
-
-			);
+DROP TABLE IF EXISTS `weekly_exercise`;
+CREATE TABLE weekly_exercise
+( weekly_exercise_id int NOT NULL,
+  weekly_exercise_name char(50) NOT NULL,
+  reps int(10),
+  CONSTRAINT weekly_exercises_pk PRIMARY KEY (weekly_exercise_id)
+);
